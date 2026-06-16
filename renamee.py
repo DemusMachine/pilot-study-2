7ebdc891579a6a750e9e7a488256b39818d3d1fa
Misha <demusmachine@gmail.com>
2026-06-16 23:52:57 +0800

    pictures

--- renamee.py
+++ renamee.py
@@ -1,9 +1,8 @@
 import os
 
 count = 1
-for file in os.listdir("D:/html-projects/pilot-study-2/images-hover"):
+for file in os.listdir("D:/html-projects/pilot-study-2/images"):
 
-    path = os.path.join("D:/html-projects/pilot-study-2/images-hover",file)
-    os.rename(path,f"D:/html-projects/pilot-study-2/images-hover/PQ{count:02d}.jpg")
-
+    path = os.path.join("D:/html-projects/pilot-study-2/images",file)
+    os.rename(path,f"D:/html-projects/pilot-study-2/images/P{count:02d}.jpg")
     count+=1
